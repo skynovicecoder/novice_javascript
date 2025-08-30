fade838e73736fa6f6479b9166fb3e6643fdcc48
SONIL KUMAR <sonil@SONILs-MacBook-Air.local>
2025-08-30 23:40:32 +0530

    checking syntax for array in javascript

--- practice/string_practice/reverse_string.js
+++ practice/string_practice/reverse_string.js
@@ -1,9 +1,12 @@
 function reverseString(str) {
   let reversed = "";
+  //let reversed=[];
   for (let i = str.length - 1; i >= 0; i--) {
     reversed += str[i];
+    //reversed[str.length-1-i] = str[i];
   }
   return reversed;
+  //return reversed.toString();
 }
 
 console.log(reverseString("Hello"));
